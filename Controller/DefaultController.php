3c85288f079a38ef9b08d8ab0f616e1d434cd293
Yuriy Korman <yuriy.korman@sigmaukraine.com>
2017-01-08 01:24:21 +0200

    change controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CeantAdminBundle:Default:index.html.twig');
+        return $this->render('CeantAdminBundle:pages:home.html.twig');
     }
 }
